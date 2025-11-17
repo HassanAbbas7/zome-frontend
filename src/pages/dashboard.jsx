f0ece4ea8baab4bf98f8d50148c06e5b4d54ec56
HassanAbbas7 <ph015016test1@gmail.com>
2025-11-17 15:13:10 +0500

    fixes

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './Dashboard.css';
+import './dashboard.css';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
